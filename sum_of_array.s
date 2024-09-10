252c4ab204d3bb7207fe474ff0fae429c9384be5
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-09-10 16:34:48 -0700

    Update sum_of_array.s

--- sum_of_array.s
+++ sum_of_array.s
@@ -7,17 +7,20 @@
 #
 # Insert your answers below each question:-
 # 1(a) What is the address location of the first element of the array, X.
-# $t0
+#      Possibly just the address X, since X is the address of the 1st element in the array.
+#      The address location could be zero since it is the first address.
 #
 # 1(b) What is the address location of the last element (element 7) of the array, X.
+#      The address location could be 8 because it is at index 2 and all elements are stored as words.
 #
 # 2.   What are the values at location 'SUM' as the program executes?
 #      0, -2, -6, 1
+#
 # 3.   Traverse the program in single-step mode. Write down the intermediate 
 #      values stored in register $s1 as the program progresses.
 #
 # 4.   What is the purpose of register $t0 in this program?
-#
+#      $t0 temporarily holds the address of the current element.
 #
 #
 .data                   	# Put Global Data here
