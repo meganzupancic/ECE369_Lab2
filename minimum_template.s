9a30d1ea2453b8b543a1fdc28a4dbfe6d061e81e
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-09-15 13:17:57 -0700

    Update minimum_template.s

--- minimum_template.s
+++ minimum_template.s
@@ -197,8 +197,8 @@ test:
     addi    $sp, $sp, -4        # Make space on stack
     sw      $ra, 0($sp)         # Save return address
 
-    jal    minimum             # call 'minimum' function
-#    jal    MaxIndex            # call 'MaxIndex' function
+#    jal    minimum             # call 'minimum' function
+    jal    MaxIndex            # call 'MaxIndex' function
     jal    print_integer       # Jump to the routine that prints the index
    
 # Comment out minimum, MaxIndex and print_integer function calls and uncomment sort and 
