b3b9593e038c8e192ecc9efd132b8e3b9b1d2f85
athiel1 <157844495+athiel1@users.noreply.github.com>
2024-09-15 14:02:05 -0700

    Update debug.s

--- debug.s
+++ debug.s
@@ -2,6 +2,7 @@
 #  Max Score: 9 points
 #
 # Students: Megan Zupancic, Annie Thiel
+# Percent Effort: 50%, 50%
 #
 # 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
